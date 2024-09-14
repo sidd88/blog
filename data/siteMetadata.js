63184c4895a05213896c8a29845cae9e68a2a25c
Do Dinh Si <dodinhsi88@gmail.com>
2024-09-15 02:13:00 +0700

    update post data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://sidd88.me',
-  siteRepo: 'https://github.com/sidd88/sidd88',
+  siteRepo: 'https://github.com/sidd88/blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
