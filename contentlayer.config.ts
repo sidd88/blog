c8886bf35332505da75346f81901963a5ed49bc2
Do Dinh Si <dodinhsi88@gmail.com>
2024-10-19 17:26:42 +0700

    revert slug config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -44,7 +44,7 @@ const computedFields: ComputedFields = {
   readingTime: { type: 'json', resolve: (doc) => readingTime(doc.body.raw) },
   slug: {
     type: 'string',
-    resolve: (doc) => getSlug(doc._raw.flattenedPath),
+    resolve: (doc) => doc._raw.flattenedPath.replace(/^.+?(\/)/, ''),
   },
   path: {
     type: 'string',
@@ -88,20 +88,6 @@ function createSearchIndex(allBlogs) {
     )
     console.log('Local search index generated...')
   }
-}
-
-/**
- * Customization function to clean the SLUG
- */
-function getSlug(slug: string): string {
-  let path = slug.replace(/^.+?(\/)/, '')
-
-  // Remove filename prefix if exist
-  if (path.includes('__')) {
-    const pathArr = path.split('__')
-    path = pathArr[pathArr.length - 1]
-  }
-  return path
 }
 
 export const Blog = defineDocumentType(() => ({
